750081d5751c50d3029ff3aea40b2a51dd073454
Juraj Michálek <juraj.michalek@espressif.com>
2021-11-11 10:35:06 +0100

    code clean up

--- Flash.ps1
+++ Flash.ps1
@@ -1,1 +1,7 @@
-esptool.py --chip esp32s2 elf2image --flash_size 2MB .\rust-esp32-std-demo -o out;  esptool.py --chip esp32s2 write_flash 0x10000 .\out; espmonitor.exe COM5
+
+$ErrorActionPreference = "Stop"
+
+esptool.py --chip esp32s2 elf2image --flash_size 2MB .\target\xtensa-esp32s2-espidf\release\rust-esp32-std-demo -o out
+esptool.py --chip esp32s2 write_flash 0x10000 .\out
+espmonitor.exe COM5
+
--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,6 @@ use embedded_svc::httpd::*;
 use embedded_svc::io;
 use embedded_svc::ipv4;
 use embedded_svc::ping::Ping;
-use embedded_svc::wifi::*;
 
 use esp_idf_svc::eth::*;
 use esp_idf_svc::httpd as idf;
@@ -31,14 +30,12 @@ use esp_idf_svc::netif::*;
 use esp_idf_svc::nvs::*;
 use esp_idf_svc::ping;
 use esp_idf_svc::sysloop::*;
-use esp_idf_svc::wifi::*;
 
 use esp_idf_hal::delay;
 use esp_idf_hal::gpio;
 use esp_idf_hal::i2c;
 use esp_idf_hal::prelude::*;
 use esp_idf_hal::spi;
-use esp_idf_hal::ulp;
 
 use esp_idf_sys;
 use esp_idf_sys::esp;
@@ -78,18 +75,7 @@ use embedded_graphics::{
     prelude::*,
 };
 
-#[allow(dead_code)]
-#[cfg(not(feature = "qemu"))]
-const SSID: &str = env!("RUST_ESP32_STD_DEMO_WIFI_SSID");
-#[allow(dead_code)]
-#[cfg(not(feature = "qemu"))]
-const PASS: &str = env!("RUST_ESP32_STD_DEMO_WIFI_PASS");
 
-#[cfg(esp32s2)]
-include!(env!("EMBUILD_GENERATED_SYMBOLS_FILE"));
-
-#[cfg(esp32s2)]
-const ULP: &[u8] = include_bytes!(env!("EMBUILD_GENERATED_BIN_FILE"));
 
 
 
@@ -269,181 +255,6 @@ where
 
     Ok(())
 }
-
-#[allow(unused_variables)]
-fn httpd(mutex: Arc<(Mutex<Option<u32>>, Condvar)>) -> Result<idf::Server> {
-    let server = idf::ServerRegistry::new()
-        .at("/")
-        .get(|_| Ok("Hello from Rust!".into()))?
-        .at("/foo")
-        .get(|_| bail!("Boo, something happened!"))?
-        .at("/bar")
-        .get(|_| {
-            Response::new(403)
-                .status_message("No permissions")
-                .body("You have no permissions to access this page".into())
-                .into()
-        })?;
-
-    #[cfg(esp32s2)]
-    let server = httpd_ulp_endpoints(server, mutex)?;
-
-    server.start(&Default::default())
-}
-
-#[cfg(esp32s2)]
-fn httpd_ulp_endpoints(
-    server: ServerRegistry,
-    mutex: Arc<(Mutex<Option<u32>>, Condvar)>,
-) -> Result<ServerRegistry> {
-    server
-        .at("/ulp")
-        .get(|_| {
-            Ok(r#"
-            <doctype html5>
-            <html>
-                <body>
-                    <form method = "post" action = "/ulp_start" enctype="application/x-www-form-urlencoded">
-                        Connect a LED to ESP32-S2 GPIO <b>Pin 04</b> and GND.<br>
-                        Blink it with ULP <input name = "cycles" type = "text" value = "10"> times
-                        <input type = "submit" value = "Go!">
-                    </form>
-                </body>
-            </html>
-            "#.into())
-        })?
-        .at("/ulp_start")
-        .post(move |mut request| {
-            let body = request.as_bytes()?;
-
-            let cycles = url::form_urlencoded::parse(&body)
-                .filter(|p| p.0 == "cycles")
-                .map(|p| str::parse::<u32>(&p.1).map_err(Error::msg))
-                .next()
-                .ok_or(anyhow!("No parameter cycles"))??;
-
-            let mut wait = mutex.0.lock().unwrap();
-
-            *wait = Some(cycles);
-            mutex.1.notify_one();
-
-            Ok(format!(
-                r#"
-                <doctype html5>
-                <html>
-                    <body>
-                        About to sleep now. The ULP chip should blink the LED {} times and then wake me up. Bye!
-                    </body>
-                </html>
-                "#,
-                cycles).to_owned().into())
-        })
-}
-
-#[cfg(esp32s2)]
-fn start_ulp(cycles: u32) -> Result<()> {
-    use esp_idf_hal::ulp;
-
-    unsafe {
-        esp!(esp_idf_sys::ulp_riscv_load_binary(
-            ULP.as_ptr(),
-            ULP.len() as _
-        ))?;
-        info!("RiscV ULP binary loaded successfully");
-
-        // Once started, the ULP will wakeup every 5 minutes
-        // TODO: Figure out how to disable ULP timer-based wakeup completely, with an ESP-IDF call
-        ulp::enable_timer(false);
-
-        info!("RiscV ULP Timer configured");
-
-        info!(
-            "Default ULP LED blink cycles: {}",
-            core::ptr::read_volatile(CYCLES as *mut u32)
-        );
-
-        core::ptr::write_volatile(CYCLES as *mut u32, cycles);
-        info!(
-            "Sent {} LED blink cycles to the ULP",
-            core::ptr::read_volatile(CYCLES as *mut u32)
-        );
-
-        esp!(esp_idf_sys::ulp_riscv_run())?;
-        info!("RiscV ULP started");
-
-        esp!(esp_idf_sys::esp_sleep_enable_ulp_wakeup())?;
-        info!("Wakeup from ULP enabled");
-
-        // Wake up by a timer in 60 seconds
-        info!("About to get to sleep now. Will wake up automatically either in 1 minute, or once the ULP has done blinking the LED");
-        esp_idf_sys::esp_deep_sleep(Duration::from_secs(60).as_micros() as u64);
-    }
-
-    Ok(())
-}
-
-#[cfg(not(feature = "qemu"))]
-#[allow(dead_code)]
-fn wifi(
-    netif_stack: Arc<EspNetifStack>,
-    sys_loop_stack: Arc<EspSysLoopStack>,
-    default_nvs: Arc<EspDefaultNvs>,
-) -> Result<Box<EspWifi>> {
-    let mut wifi = Box::new(EspWifi::new(netif_stack, sys_loop_stack, default_nvs)?);
-
-    info!("Wifi created, about to scan");
-
-    let ap_infos = wifi.scan()?;
-
-    let ours = ap_infos.into_iter().find(|a| a.ssid == SSID);
-
-    let channel = if let Some(ours) = ours {
-        info!(
-            "Found configured access point {} on channel {}",
-            SSID, ours.channel
-        );
-        Some(ours.channel)
-    } else {
-        info!(
-            "Configured access point {} not found during scanning, will go with unknown channel",
-            SSID
-        );
-        None
-    };
-
-    wifi.set_configuration(&Configuration::Mixed(
-        ClientConfiguration {
-            ssid: SSID.into(),
-            password: PASS.into(),
-            channel,
-            ..Default::default()
-        },
-        AccessPointConfiguration {
-            ssid: "aptest".into(),
-            channel: channel.unwrap_or(1),
-            ..Default::default()
-        },
-    ))?;
-
-    info!("Wifi configuration set, about to get status");
-
-    let status = wifi.get_status();
-
-    if let Status(
-        ClientStatus::Started(ClientConnectionStatus::Connected(ClientIpStatus::Done(ip_settings))),
-        ApStatus::Started(ApIpStatus::Done),
-    ) = status
-    {
-        info!("Wifi connected");
-
-        ping(&ip_settings)?;
-    } else {
-        bail!("Unexpected Wifi status: {:?}", status);
-    }
-
-    Ok(wifi)
-}
-
 #[cfg(any(feature = "qemu", feature = "w5500", feature = "ip101"))]
 fn eth_configure<HW>(mut eth: Box<EspEth<HW>>) -> Result<Box<EspEth<HW>>> {
     info!("Eth created");
@@ -485,15 +296,6 @@ fn ping(ip_settings: &ipv4::ClientSettings) -> Result<()> {
     Ok(())
 }
 
-#[cfg(not(feature = "qemu"))]
-#[cfg(esp_idf_config_lwip_ipv4_napt)]
-fn enable_napt(wifi: &mut EspWifi) -> Result<()> {
-    wifi.with_router_netif_mut(|netif| netif.unwrap().enable_napt(true));
-
-    info!("NAPT enabled on the WiFi SoftAP!");
-
-    Ok(())
-}
 
 // Kaluga needs customized screen orientation commands
 // (not a surprise; quite a few ILI9341 boards need these as evidences in the TFT_eSPI & lvgl ESP32 C drivers)
