abe6416fe70be4338c87e3a4bd2b02fd911ebc7b
Juraj Michálek <juraj.michalek@espressif.com>
2022-05-05 17:38:54 +0200

    tool: register Wokwi project ID

--- run-wokwi.sh
+++ run-wokwi.sh
@@ -10,6 +10,6 @@ find target -name bootloader.bin -exec cp {} . \;
 find target -name partition-table.bin -exec cp {} . \;
 
 cd ~/esp32-wokwi-gitpod-websocket-server/
+export WOKWI_PROJECT_ID="330831847505265234"
 python3 server.py
-
 
