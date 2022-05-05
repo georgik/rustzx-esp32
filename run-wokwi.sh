a2cba6eaf916047beb6061161d45f341ca4d2b67
Juraj Michálek <juraj.michalek@espressif.com>
2022-05-05 17:52:20 +0200

    ci: change Wokwi build to Kaluga with ili

--- run-wokwi.sh
+++ run-wokwi.sh
@@ -4,7 +4,7 @@ set -e
 
 pip3 install websockets==10.2
 
-cargo +esp espflash save-image app.bin --target xtensa-esp32s2-espidf --release --features "esp32s2_usb_otg native"
+cargo +esp espflash save-image app.bin --target xtensa-esp32s2-espidf --release --features "kaluga_ili9341 native"
 
 find target -name bootloader.bin -exec cp {} . \;
 find target -name partition-table.bin -exec cp {} . \;
