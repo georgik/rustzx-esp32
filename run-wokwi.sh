fba1afa2e385849f56a8369512d67df3e9a82e43
Juraj Michálek <juraj.michalek@espressif.com>
2022-05-05 14:47:29 +0200

    ci: websockets must be deployed to activated python environment

--- run-wokwi.sh
+++ run-wokwi.sh
@@ -3,4 +3,11 @@
 set -e
 
 cargo +esp espflash save-image app.bin --target xtensa-esp32s2-espidf --release --features "esp32s2_usb_otg native"
-python3 ~/esp32-wokwi-gitpod-websocket-server/server.py
+
+find target -name bootloader.bin -exec cp {} . \;
+find target -name partition-table.bin -exec cp {} . \;
+
+cd ~/esp32-wokwi-gitpod-websocket-server/
+python3 server.py
+
+
