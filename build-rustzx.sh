aea5d281fccaef3566a00b2fbae82a1c25c619c3
Juraj Michálek <juraj.michalek@espressif.com>
2022-05-10 19:58:01 +0200

    tools: add esp32 support

--- build-rustzx.sh
+++ build-rustzx.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-#PORT=`ls /dev/tty.usbserial-*01 | head -n 1`
-# Build and flash using cargo-espflash
-#echo "Using port: ${PORT}"
-#cargo +esp espflash --target xtensa-esp32s2-espidf --release --features "kaluga_ili9341 native" ${PORT} --monitor
-#cargo +esp build --target xtensa-esp32s2-espidf --release --features "kaluga_ili9341 native"
+#cargo +esp espflash --target xtensa-esp32s2-espidf --release --features "kaluga_ili9341" --monitor
+#cargo +esp build --target xtensa-esp32s2-espidf --release --features "kaluga_ili9341"
 #cargo +esp build --target xtensa-esp32s3-espidf --release --features "esp32s3_usb_otg"
-cargo +esp build --target riscv32imc-esp-espidf --release --features "esp32c3_ili9341"
+cargo +esp build --target xtensa-esp32-espidf --release --features "esp32_ili9341"
+#cargo +esp build --target riscv32imc-esp-espidf --release --features "esp32c3_ili9341"
 
