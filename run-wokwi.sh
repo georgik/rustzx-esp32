41a4064d4b3890e266e56f352bbe501321a44170
Juraj Michalek <juraj.michalek@espressif.com>
2022-05-12 12:21:36 +0200

    tools: specify offset for application

--- run-wokwi.sh
+++ run-wokwi.sh
@@ -4,7 +4,7 @@ set -e
 
 pip3 install websockets==10.2
 
-cargo +esp espflash save-image app.bin --target riscv32imc-esp-espidf --release --features "esp32c3_ili9341"
+cargo +esp espflash save-image app.bin --target xtensa-esp32-espidf --release --features "esp32_ili9341"
 
 find target -name bootloader.bin -exec cp {} . \;
 find target -name partition-table.bin -exec cp {} . \;
@@ -14,6 +14,10 @@ cd ~/esp32-wokwi-gitpod-websocket-server/
 # ESP32S2 board
 #export WOKWI_PROJECT_ID="330831847505265234"
 # ESP32C3
-export WOKWI_PROJECT_ID="330910629554553426"
+export WOKWI_PROJECT_ID="331440829570744915"
+export ESP_BOOTLOADER_OFFSET="0x1000"
+export ESP_PARTITION_TABLE_OFFSET="0x8000"
+export ESP_APP_OFFSET="0x10000"
 
 python3 server.py
+
