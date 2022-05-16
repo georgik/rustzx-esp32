624bd1bf5890b0cd0ff5c82c75e0a5f488939a85
Juraj Michalek <juraj.michalek@espressif.com>
2022-05-16 11:03:14 +0200

    ci: add debug support for Wokwi

--- build-rustzx.sh
+++ build-rustzx.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-#cargo +esp espflash --target xtensa-esp32s2-espidf --release --features "kaluga_ili9341" --monitor
-#cargo +esp build --target xtensa-esp32s2-espidf --release --features "kaluga_ili9341"
-#cargo +esp build --target xtensa-esp32s3-espidf --release --features "esp32s3_usb_otg"
 cargo +esp build --target xtensa-esp32-espidf --release --features "esp32_ili9341"
-#cargo +esp build --target riscv32imc-esp-espidf --release --features "esp32c3_ili9341"
-
--- debug-wokwi.sh
+++ debug-wokwi.sh
@@ -5,10 +5,10 @@ set -e
 pip3 install websockets==10.2
 
 export ESP_ARCH="xtensa-esp32-espidf"
-cargo +esp espflash save-image app.bin --target "${ESP_ARCH}" --release --features "esp32_ili9341"
+cargo +esp espflash save-image app.bin --target "${ESP_ARCH}" --features "esp32_ili9341"
 
-find target/${ESP_ARCH}/release -name bootloader.bin -exec cp {} . \;
-find target/${ESP_ARCH}/release -name partition-table.bin -exec cp {} . \;
+find target/${ESP_ARCH}/debug -name bootloader.bin -exec cp {} . \;
+find target/${ESP_ARCH}/debug -name partition-table.bin -exec cp {} . \;
 
 cd ~/esp32-wokwi-gitpod-websocket-server/
 
@@ -17,5 +17,6 @@ export WOKWI_PROJECT_ID="331440829570744915"
 export ESP_BOOTLOADER_OFFSET="0x1000"
 export ESP_PARTITION_TABLE_OFFSET="0x8000"
 export ESP_APP_OFFSET="0x10000"
+export ELF_FILE="rustzx-esp32"
 
 python3 server.py
