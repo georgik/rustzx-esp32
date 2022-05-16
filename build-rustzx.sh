60b6a08aa695b7c9fa1d9c0c928f206f2122105d
Juraj Michálek <juraj.michalek@gmail.com>
2022-05-16 14:57:03 +0200

    Feature/devcontainer wokwi (#10)
    * Improve devcontainers (#9)
    - Add support for DevContainers debugging
    - Add support for GitPod debugging
    
    Co-authored-by: Sergio Gasquez Arcos <sergio.gasquez@gmail.com>

--- build-rustzx.sh
+++ build-rustzx.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# Gitpod tasks do not source the user environment
+if [ "${USER}" == "gitpod" ]; then
+    source /home/gitpod/export-rust.sh > /dev/null 2>&1
+    export IDF_TOOLS_PATH=/home/gitpod/.espressif
+    source /home/gitpod/.espressif/frameworks/esp-idf-v4.4/export.sh > /dev/null 2>&1
+    export CURRENT_PROJECT=rustzx-esp32
+fi
+
 cargo +esp build --target xtensa-esp32-espidf --release --features "esp32_ili9341"
