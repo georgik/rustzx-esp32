bbcc64e2f9f1dbce5a7a5382b67732ba320709bd
Juraj Michálek <juraj.michalek@espressif.com>
2022-05-17 10:36:34 +0200

    ci: fix wokwi project ID

--- run-wokwi.sh
+++ run-wokwi.sh
@@ -19,14 +19,15 @@ pip3 install websockets==10.2
 # ESP32 board
 export ESP_BOARD="esp32c3"
 export ESP_ELF="rustzx-esp32"
-export WOKWI_PROJECT_ID="331440829570744915"
 
 if [ "${ESP_BOARD}" == "esp32c3" ]; then
+    export WOKWI_PROJECT_ID="330910629554553426"
     export ESP_ARCH="riscv32imc-esp-espidf"
     export ESP_BOOTLOADER_OFFSET="0x0000"
     export ESP_PARTITION_TABLE_OFFSET="0x8000"
     export ESP_APP_OFFSET="0x10000"
 else
+    export WOKWI_PROJECT_ID="331440829570744915"
     export ESP_ARCH="xtensa-esp32-espidf"
     export ESP_BOOTLOADER_OFFSET="0x1000"
     export ESP_PARTITION_TABLE_OFFSET="0x8000"
