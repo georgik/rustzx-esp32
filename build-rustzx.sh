998c68e0e3627112225dbf5d4c1c1b81f9f3f9d8
Sergio Gasquez <sergio.gasquez@gmail.com>
2022-05-23 14:02:24 +0000

    Unset CURRENT_PROJECT

--- build-rustzx.sh
+++ build-rustzx.sh
@@ -2,17 +2,24 @@
 
 # Gitpod and VsCode Codespaces tasks do not source the user environment
 if [ "${USER}" == "gitpod" ]; then
-    export CURRENT_PROJECT=/workspace/rustzx-esp32
     which idf.py >/dev/null || {
         source ~/export-rust.sh > /dev/null 2>&1
     }
 elif [ "${CODESPACE_NAME}" != "" ]; then
-    export CURRENT_PROJECT=/workspaces/rustzx-esp32
     which idf.py >/dev/null || {
         source ~/export-rust.sh > /dev/null 2>&1
     }
-else
-    export CURRENT_PROJECT=~/workspace
 fi
 
-cargo +esp build --target xtensa-esp32-espidf --release --features "esp32_ili9341"
+
+case "$1" in
+    ""|"release")
+        cargo +esp build --target xtensa-esp32-espidf --release --features "esp32_ili9341"
+        ;;
+    "debug")
+        cargo +esp build --target xtensa-esp32-espidf --features "esp32_ili9341"
+        ;;
+    *)
+        echo "Wrong argument. Only \"debug\"/\"release\" arguments are supported"
+        exit 1;;
+esac
