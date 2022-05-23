6abb1b9ffa14eb9ea871416c8b68e5aa1323db8d
Juraj Michalek <juraj.michalek@espressif.com>
2022-05-23 14:12:57 +0200

    ci: fix partition offset for debug

--- debug-wokwi.sh
+++ debug-wokwi.sh
@@ -29,7 +29,7 @@ if [ "${ESP_BOARD}" == "esp32c3" ]; then
 elif [ "${ESP_BOARD}" == "esp32s2" ]; then
     export WOKWI_PROJECT_ID="330831847505265234"
     export ESP_ARCH="xtensa-esp32s2-espidf"
-    export ESP_BOOTLOADER_OFFSET="0x0000"
+    export ESP_BOOTLOADER_OFFSET="0x1000"
     export ESP_PARTITION_TABLE_OFFSET="0x8000"
     export ESP_APP_OFFSET="0x10000"
 else
