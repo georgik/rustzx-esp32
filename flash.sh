b18bb50dfbbb7f97c6103ad27ca4032a2f8bea9f
Sergio Gasquez Arcos <sergio.gasquez@gmail.com>
2022-05-24 03:15:51 +0200

    Codespaces integration. Swicht to web-flash and wokwi-server tools (#14)
    * Use web-flash and wokwi-server crates
    * Add environment for gh codespaces

--- flash.sh
+++ flash.sh
@@ -26,26 +26,14 @@ case "$1" in
 esac
 
 
-if [ "${USER}" == "gitpod" ];then
-    gp_url=$(gp url 9012)
-    echo "gp_url=${gp_url}"
-    export WOKWI_HOST=${gp_url:8}
-elif [ "${CODESPACE_NAME}" != "" ];then
-    export WOKWI_HOST=${CODESPACE_NAME}-9012.githubpreview.dev
-fi
-
-export ESP_BOARD="esp32"
 export ESP_ELF="rustzx-esp32"
-export WOKWI_PROJECT_ID="331440829570744915"
+export ESP_BOARD="esp32"
 if [ "${ESP_BOARD}" == "esp32c3" ]; then
     export ESP_ARCH="riscv32imc-esp-espidf"
-    export WOKWI_PROJECT_ID="330910629554553426"
 elif [ "${ESP_BOARD}" == "esp32s2" ]; then
-    export WOKWI_PROJECT_ID="330831847505265234"
     export ESP_ARCH="xtensa-esp32s2-espidf"
 else
-    export WOKWI_PROJECT_ID="331440829570744915"
     export ESP_ARCH="xtensa-esp32-espidf"
 fi
 
-wokwi-server --chip ${ESP_BOARD} --id ${WOKWI_PROJECT_ID} ${CURRENT_PROJECT}/target/${ESP_ARCH}/${BUILD_MODE}/${ESP_ELF}
+web-flash --chip ${ESP_BOARD} ${CURRENT_PROJECT}/target/${ESP_ARCH}/${BUILD_MODE}/${ESP_ELF}
