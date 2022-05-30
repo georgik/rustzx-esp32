e526211c53a15b1a799dda6c885b1781b119e3ab
Juraj Michálek <juraj.michalek@espressif.com>
2022-05-30 13:26:25 +0000

    map ascii table to ZX

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,15 @@ use std::io::Read;
 use std::io::Write;
 use std::result::Result::Ok;
 
+/// This configuration is picked up at compile time by `build.rs` from the
+/// file `cfg.toml`.
+#[toml_cfg::toml_config]
+pub struct Config {
+    #[default("Wokwi-GUEST")]
+    wifi_ssid: &'static str,
+    #[default("")]
+    wifi_psk: &'static str,
+}
 
 fn main() -> Result<()> {
     esp_idf_sys::link_patches();
@@ -56,20 +65,18 @@ fn handle_client( mut stream: TcpStream) -> u8 {
     // 0
 }
 
-const SSID: &str = "";
-const PASS: &str = "";
-
 #[allow(dead_code)]
 fn wifi(
     netif_stack: Arc<EspNetifStack>,
     sys_loop_stack: Arc<EspSysLoopStack>,
     default_nvs: Arc<EspDefaultNvs>,
 ) -> anyhow::Result<Box<EspWifi>> {
+    let app_config = CONFIG;
     let mut wifi = Box::new(EspWifi::new(netif_stack, sys_loop_stack, default_nvs)?);
 
     wifi.set_configuration(&Configuration::Client(ClientConfiguration {
-        ssid: SSID.into(),
-        password: PASS.into(),
+        ssid: app_config.wifi_ssid.into(),
+        password: app_config.wifi_psk.into(),
         auth_method: AuthMethod::None,
         ..Default::default()
     }))?;
@@ -98,6 +105,90 @@ fn wifi(
     Ok(wifi)
 }
 
+
+/// returns ZX Spectum key form scancode of None if not found
+fn ascii_code_to_zxkey(ascii_code: u8) -> Option<ZXKey> {
+    let zxkey = match ascii_code {
+        // Control keys
+        0x10 => Some(ZXKey::Enter),
+
+        // Symbols
+        0x20 => Some(ZXKey::Space), // Space
+        0x22 => Some(ZXKey::P),     // "
+
+        // Numbers 0-9
+        0x30 => Some(ZXKey::N0),
+        0x31 => Some(ZXKey::N1),
+        0x32 => Some(ZXKey::N2),
+        0x33 => Some(ZXKey::N3),
+        0x34 => Some(ZXKey::N4),
+        0x35 => Some(ZXKey::N5),
+        0x36 => Some(ZXKey::N6),
+        0x37 => Some(ZXKey::N7),
+        0x38 => Some(ZXKey::N8),
+        0x39 => Some(ZXKey::N9),
+
+        // Upper-case letters A-Z
+        0x41 => Some(ZXKey::A),
+        0x42 => Some(ZXKey::B),
+        0x43 => Some(ZXKey::C),
+        0x44 => Some(ZXKey::D),
+        0x45 => Some(ZXKey::E),
+        0x46 => Some(ZXKey::F),
+        0x47 => Some(ZXKey::G),
+        0x48 => Some(ZXKey::H),
+        0x49 => Some(ZXKey::I),
+        0x4A => Some(ZXKey::J),
+        0x4B => Some(ZXKey::K),
+        0x4C => Some(ZXKey::L),
+        0x4D => Some(ZXKey::M),
+        0x4E => Some(ZXKey::N),
+        0x4F => Some(ZXKey::O),
+        0x50 => Some(ZXKey::P),
+        0x51 => Some(ZXKey::Q),
+        0x52 => Some(ZXKey::R),
+        0x53 => Some(ZXKey::S),
+        0x54 => Some(ZXKey::T),
+        0x55 => Some(ZXKey::U),
+        0x56 => Some(ZXKey::V),
+        0x57 => Some(ZXKey::W),
+        0x58 => Some(ZXKey::X),
+        0x59 => Some(ZXKey::Y),
+        0x5A => Some(ZXKey::Z),
+
+        // Lowe-case letters - a-z
+        0x61 => Some(ZXKey::A),
+        0x62 => Some(ZXKey::B),
+        0x63 => Some(ZXKey::C),
+        0x64 => Some(ZXKey::D),
+        0x65 => Some(ZXKey::E),
+        0x66 => Some(ZXKey::F),
+        0x67 => Some(ZXKey::G),
+        0x68 => Some(ZXKey::H),
+        0x69 => Some(ZXKey::I),
+        0x6A => Some(ZXKey::J),
+        0x6B => Some(ZXKey::K),
+        0x6C => Some(ZXKey::L),
+        0x6D => Some(ZXKey::M),
+        0x6E => Some(ZXKey::N),
+        0x6F => Some(ZXKey::O),
+        0x70 => Some(ZXKey::P),
+        0x71 => Some(ZXKey::Q),
+        0x72 => Some(ZXKey::R),
+        0x73 => Some(ZXKey::S),
+        0x74 => Some(ZXKey::T),
+        0x75 => Some(ZXKey::U),
+        0x76 => Some(ZXKey::V),
+        0x77 => Some(ZXKey::W),
+        0x78 => Some(ZXKey::X),
+        0x79 => Some(ZXKey::Y),
+        0x7A => Some(ZXKey::Z),
+
+        _ => None,
+    };
+
+    zxkey
+}
 
 fn emulate_zx<D>(mut display: D, color_conv: fn(ZXColor, ZXBrightness) -> D::Color) -> Result<()>
 where
@@ -138,10 +229,7 @@ where
 
     let listener = TcpListener::bind("0.0.0.0:80").unwrap();
     listener.set_nonblocking(true).expect("Cannot set non-blocking");
-    // accept connections and process them serially
-    // for stream in listener.incoming() {
-    //     handle_client(stream?);
-    // }
+
     loop {
         const MAX_FRAME_DURATION: Duration = Duration::from_millis(0);
 
@@ -160,37 +248,39 @@ where
               Ok(stream) => {
                   let key = handle_client(stream);
                   println!("Key: {}", key);
-                  match key {
-                    0x41 => { emulator.send_key(ZXKey::A, true); }
-                    0x42 => { emulator.send_key(ZXKey::B, true); }
-                    0x43 => { emulator.send_key(ZXKey::C, true); }
-                    0x44 => { emulator.send_key(ZXKey::D, true); }
-                    0x45 => { emulator.send_key(ZXKey::E, true); }
-                    _ => { println!("unknown key mapping"); }
+                  let mapped_key = ascii_code_to_zxkey(key).unwrap();
+                  
+                  if key == 0x22 {
+                    emulator.send_key(ZXKey::SymShift, true);
                   }
-            emulator
-            .emulate_frames(MAX_FRAME_DURATION);
-                  emulator
-            .screen_buffer()
-            .blit(&mut display, color_conv);
-                  match key {
-                    0x41 => { emulator.send_key(ZXKey::A, false); }
-                    0x42 => { emulator.send_key(ZXKey::B, false); }
-                    0x43 => { emulator.send_key(ZXKey::C, false); }
-                    0x44 => { emulator.send_key(ZXKey::D, false); }
-                    0x45 => { emulator.send_key(ZXKey::E, false); }
-                    _ => { println!("unknown key mapping"); }
+
+                  if key >= 0x41 && key <= 0x5A {
+                    emulator.send_key(ZXKey::Shift, true);
                   }
+
+                  emulator.send_key(mapped_key, true);
+
+                  emulator.emulate_frames(MAX_FRAME_DURATION);
+                  emulator.screen_buffer()
+                    .blit(&mut display, color_conv);
+
+                  emulator.send_key(mapped_key, false);
+
+                  if key == 0x22 {
+                    emulator.send_key(ZXKey::SymShift, false);
+                  }
+
+                  if key >= 0x41 && key <= 0x5A {
+                    emulator.send_key(ZXKey::Shift, false);
+                  }
+
                 }
 
               Err(e) => {}
           }
-          emulator
-          .emulate_frames(MAX_FRAME_DURATION);
-                emulator
-          .screen_buffer()
+          emulator.emulate_frames(MAX_FRAME_DURATION);
+          emulator.screen_buffer()
           .blit(&mut display, color_conv);
-
       }
 
 
