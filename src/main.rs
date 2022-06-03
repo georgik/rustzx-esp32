16d8e9ff2edcc988a94c281a20bba6565ba0a66f
Juraj Michálek <juraj.michalek@espressif.com>
2022-06-03 09:00:50 +0000

    experimental add PS2 keyboard - not working

--- src/main.rs
+++ src/main.rs
@@ -50,8 +50,156 @@ fn main() -> Result<()> {
 
     let peripherals = Peripherals::take().unwrap();
 
-    emulate_zx(display::create!(peripherals)?, display::color_conv)
+    emulate_zx(peripherals, display::create!(peripherals)?, display::color_conv)
 }
+
+
+
+
+
+
+use core::cell::RefCell;
+
+use core::mem::MaybeUninit;
+use esp32_hal::{
+    gpio::{Gpio26, Gpio27},
+    pac::Peripherals,
+    prelude::*,
+    RtcCntl, Timer, IO,
+};
+use esp_hal_common::{interrupt, pac, Cpu, Event, Floating, Input, Pin};
+// use esp_println::println;
+// use panic_halt as _;
+use pc_keyboard::{layouts, HandleControl, ScancodeSet2};
+// use riscv::interrupt::Mutex;
+// use riscv_rt::entry;
+
+static mut CLK: Mutex<RefCell<Option<Gpio26<Input<Floating>>>>> = Mutex::new(RefCell::new(None));
+static mut DATA: Mutex<RefCell<Option<Gpio27<Input<Floating>>>>> = Mutex::new(RefCell::new(None));
+
+
+
+static mut QUEUE: Option<SimpleQueue<u8, 5>> = None;
+
+fn send_byte(byte: u8) {
+    riscv::interrupt::free(|_| unsafe {
+        if QUEUE.is_none() {
+            QUEUE = Some(SimpleQueue::new());
+        }
+        match QUEUE {
+            Some(ref mut queue) => {
+                queue.enqueue(byte);
+            }
+            None => (),
+        }
+    });
+}
+
+fn get_byte() -> Option<u8> {
+    riscv::interrupt::free(|_| unsafe {
+        match QUEUE {
+            Some(ref mut queue) => queue.dequeue(),
+            None => None,
+        }
+    })
+}
+
+#[no_mangle]
+pub fn interrupt3() {
+    static mut BIT_COUNT: usize = 0;
+    static mut CURRENT: u8 = 0;
+
+    riscv::interrupt::free(|cs| unsafe {
+        let mut clk = CLK.borrow(*cs).borrow_mut();
+        let clk = clk.as_mut().unwrap();
+
+        let mut data = DATA.borrow(*cs).borrow_mut();
+        let data = data.as_mut().unwrap();
+
+        let bit = if data.is_high().unwrap() { 1 } else { 0 };
+
+        interrupt::clear(Cpu::ProCpu, interrupt::CpuInterrupt::Interrupt3);
+        clk.clear_interrupt();
+
+        if BIT_COUNT > 0 && BIT_COUNT < 9 {
+            CURRENT = CURRENT.overflowing_shr(1).0;
+            CURRENT |= bit << 7;
+        }
+        BIT_COUNT += 1;
+
+        if BIT_COUNT == 11 {
+            send_byte(CURRENT);
+
+            BIT_COUNT = 0;
+            CURRENT = 0;
+        }
+    });
+}
+
+pub struct SimpleQueue<T, const N: usize> {
+    data: [Option<T>; N],
+    read_index: usize,
+    write_index: usize,
+}
+
+impl<T, const N: usize> SimpleQueue<T, N> {
+    pub fn new() -> SimpleQueue<T, N> {
+        let mut queue = unsafe {
+            SimpleQueue {
+                data: MaybeUninit::uninit().assume_init(),
+                read_index: 0,
+                write_index: 0,
+            }
+        };
+
+        for i in 0..N {
+            queue.data[i] = None;
+        }
+
+        queue
+    }
+
+    pub fn enqueue(&mut self, e: T) -> bool {
+        self.data[self.write_index] = Some(e);
+
+        self.write_index += 1;
+        self.write_index %= N;
+
+        if self.write_index == self.read_index {
+            return false;
+        }
+
+        true
+    }
+
+    pub fn dequeue(&mut self) -> Option<T> {
+        if self.write_index == self.read_index {
+            None
+        } else {
+            let result = self.data[self.read_index].take();
+            self.read_index += 1;
+            self.read_index %= N;
+            result
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.read_index == self.write_index
+    }
+
+    pub fn is_full(&self) -> bool {
+        let mut next_write = self.read_index + 1;
+        next_write %= N;
+
+        next_write == self.read_index
+    }
+}
+
+
+
+
+
+
 
 use std::sync::mpsc::{channel, Sender, Receiver}; 
 
@@ -127,7 +275,7 @@ fn wifi(
 }
 
 
-pub enum Event {
+pub enum ZXEvent {
     ZXKey(ZXKey, bool),
     ZXKeyWithModifier(ZXKey, ZXKey, bool),
     CompoundKey(CompoundKey, bool),
@@ -148,7 +296,7 @@ pub enum Event {
 }
 
 /// returns ZX Spectum key form scancode of None if not found
-fn ascii_code_to_zxkey(ascii_code: u8, pressed: bool) -> Option<Event> {
+fn ascii_code_to_zxkey(ascii_code: u8, pressed: bool) -> Option<ZXEvent> {
     let zxkey_event = match ascii_code {
         // Control keys
         0x10 => Some(ZXKey::Enter),
@@ -199,12 +347,12 @@ fn ascii_code_to_zxkey(ascii_code: u8, pressed: bool) -> Option<Event> {
         _ => None,
     };
 
-    zxkey_event.map(|k| Event::ZXKey(k, pressed))
+    zxkey_event.map(|k| ZXEvent::ZXKey(k, pressed))
 }
 
 
 /// returns ZX Spectum key form scancode of None if not found
-fn ascii_code_to_modifier(ascii_code: u8, pressed: bool) -> Option<Event> {
+fn ascii_code_to_modifier(ascii_code: u8, pressed: bool) -> Option<ZXEvent> {
     let zxkey_event = match ascii_code {
         // Symbols
         0x21 => Some((ZXKey::SymShift, ZXKey::N1)),    // !
@@ -254,12 +402,12 @@ fn ascii_code_to_modifier(ascii_code: u8, pressed: bool) -> Option<Event> {
         _ => None,
     };
 
-    zxkey_event.map(|(k, k2)| Event::ZXKeyWithModifier(k, k2, pressed))
+    zxkey_event.map(|(k, k2)| ZXEvent::ZXKeyWithModifier(k, k2, pressed))
 }
 
 
 
-fn emulate_zx<D>(mut display: D, color_conv: fn(ZXColor, ZXBrightness) -> D::Color) -> Result<()>
+fn emulate_zx<D>(peripherals: Peripherals, mut display: D, color_conv: fn(ZXColor, ZXBrightness) -> D::Color) -> Result<()>
 where
     D: DrawTarget + Dimensions + Send + 'static,
     D::Error: core::fmt::Debug,
@@ -295,6 +443,87 @@ where
         sys_loop_stack.clone(),
         default_nvs.clone(),
     )?;
+
+
+
+
+
+
+    // let peripherals = Peripherals::take().unwrap();
+
+    // Disable the watchdog timers. For the ESP32-C3, this includes the Super WDT,
+    // the RTC WDT, and the TIMG WDTs.
+    let mut rtc_cntl = RtcCntl::new(peripherals.RTC_CNTL);
+    let mut timer0 = Timer::new(peripherals.TIMG0);
+    let mut timer1 = Timer::new(peripherals.TIMG1);
+
+    rtc_cntl.set_super_wdt_enable(false);
+    rtc_cntl.set_wdt_enable(false);
+    timer0.disable();
+    timer1.disable();
+
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let mut data_out = io.pins.gpio12.into_open_drain_output();
+    let mut clk_out = io.pins.gpio14.into_open_drain_output();
+
+    let data_in = io.pins.gpio27.into_floating_input();
+    let mut clk_in = io.pins.gpio26.into_floating_input();
+    clk_in.listen(Event::FallingEdge);
+
+    data_out.set_low().unwrap();
+    clk_out.set_low().unwrap();
+
+    data_out.set_high().unwrap();
+    clk_out.set_high().unwrap();
+
+    riscv::interrupt::free(|_cs| unsafe {
+        CLK.get_mut().replace(Some(clk_in));
+        DATA.get_mut().replace(Some(data_in));
+    });
+
+    interrupt::enable(
+        Cpu::ProCpu,
+        pac::Interrupt::GPIO,
+        interrupt::CpuInterrupt::Interrupt3,
+    );
+    interrupt::set_kind(
+        Cpu::ProCpu,
+        interrupt::CpuInterrupt::Interrupt3,
+        interrupt::InterruptKind::Level,
+    );
+    interrupt::set_priority(
+        Cpu::ProCpu,
+        interrupt::CpuInterrupt::Interrupt3,
+        interrupt::Priority::Priority1,
+    );
+
+    unsafe {
+        riscv::interrupt::enable();
+    }
+
+    let mut kb = pc_keyboard::Keyboard::new(
+        layouts::Us104Key,
+        ScancodeSet2,
+        HandleControl::MapLettersToUnicode,
+    );
+    loop {
+        if let Some(byte) = get_byte() {
+            match kb.add_byte(byte) {
+                Ok(Some(event)) => {
+                    println!("Event {:?}", event);
+                }
+                Ok(None) => (),
+                Err(e) => {
+                    println!("Error decoding: {:?}", e);
+                }
+            }
+        }
+    }
+
+
+
+
+
 
     let listener = TcpListener::bind("0.0.0.0:80").unwrap();
     listener.set_nonblocking(true).expect("Cannot set non-blocking");
@@ -345,10 +574,10 @@ where
                 .or_else(|| ascii_code_to_modifier(key, false)).unwrap();
                             
                 match mapped_key_down {
-                    Event::ZXKey(k,p) => {
+                    ZXEvent::ZXKey(k,p) => {
                         emulator.send_key(k, p);        
                     },
-                    Event::ZXKeyWithModifier(k, k2, p) => {
+                    ZXEvent::ZXKeyWithModifier(k, k2, p) => {
                         emulator.send_key(k, p);
                         emulator.send_key(k2, p);
                     }
@@ -360,10 +589,10 @@ where
                     .blit(&mut display, color_conv);
 
                 match mapped_key_up {
-                    Event::ZXKey(k,p) => {
+                    ZXEvent::ZXKey(k,p) => {
                         emulator.send_key(k, p);        
                     },
-                    Event::ZXKeyWithModifier(k, k2, p) => {
+                    ZXEvent::ZXKeyWithModifier(k, k2, p) => {
                         emulator.send_key(k, p);
                         emulator.send_key(k2, p);
                     }
