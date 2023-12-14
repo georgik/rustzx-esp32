9e636e41c66ce9d0582b959fa688aba9edd9d5c4
Juraj Michalek <juraj.michalek@gmail.com>
2023-12-14 09:36:02 +0100

    more accurate color mapping

--- m5stack-cores3/src/main.rs
+++ m5stack-cores3/src/main.rs
@@ -219,17 +219,44 @@ fn main() -> ! {
 
 }
 
+const ZX_BLACK: Rgb565 = Rgb565::BLACK;
+const ZX_BRIGHT_BLUE: Rgb565 = Rgb565::new(0, 0, Rgb565::MAX_B);
+const ZX_BRIGHT_RED: Rgb565 = Rgb565::new(Rgb565::MAX_R, 0, 0);
+const ZX_BRIGHT_PURPLE: Rgb565 = Rgb565::new(Rgb565::MAX_R, 0, Rgb565::MAX_B);
+const ZX_BRIGHT_GREEN: Rgb565 = Rgb565::new(0, Rgb565::MAX_G, 0);
+const ZX_BRIGHT_CYAN: Rgb565 = Rgb565::new(0, Rgb565::MAX_G, Rgb565::MAX_B);
+const ZX_BRIGHT_YELLOW: Rgb565 = Rgb565::new(Rgb565::MAX_R, Rgb565::MAX_G, 0);
+const ZX_BRIGHT_WHITE: Rgb565 = Rgb565::WHITE;
 
-fn color_conv(color: &ZXColor, _brightness: ZXBrightness) -> Rgb565 {
-    match color {
-        ZXColor::Black => Rgb565::BLACK,
-        ZXColor::Blue => Rgb565::BLUE,
-        ZXColor::Red => Rgb565::RED,
-        ZXColor::Purple => Rgb565::MAGENTA,
-        ZXColor::Green => Rgb565::GREEN,
-        ZXColor::Cyan => Rgb565::CYAN,
-        ZXColor::Yellow => Rgb565::YELLOW,
-        ZXColor::White => Rgb565::WHITE,
+const ZX_NORMAL_BLUE: Rgb565 = Rgb565::new(0, 0, Rgb565::MAX_B / 2);
+const ZX_NORMAL_RED: Rgb565 = Rgb565::new(Rgb565::MAX_R / 2, 0, 0);
+const ZX_NORMAL_PURPLE: Rgb565 = Rgb565::new(Rgb565::MAX_R / 2, 0, Rgb565::MAX_B / 2);
+const ZX_NORMAL_GREEN: Rgb565 = Rgb565::new(0, Rgb565::MAX_G / 2, 0);
+const ZX_NORMAL_CYAN: Rgb565 = Rgb565::new(0, Rgb565::MAX_G / 2, Rgb565::MAX_B / 2);
+const ZX_NORMAL_YELLOW: Rgb565 = Rgb565::new(Rgb565::MAX_R / 2, Rgb565::MAX_G / 2, 0);
+const ZX_NORMAL_WHITE: Rgb565 = Rgb565::new(Rgb565::MAX_R / 2, Rgb565::MAX_G / 2, Rgb565::MAX_B / 2);
+
+fn color_conv(color: &ZXColor, brightness: ZXBrightness) -> Rgb565 {
+    match (color, brightness) {
+        (ZXColor::Black, _) => ZX_BLACK,
+
+        // Bright Colors
+        (ZXColor::Blue, ZXBrightness::Bright) => ZX_BRIGHT_BLUE,
+        (ZXColor::Red, ZXBrightness::Bright) => ZX_BRIGHT_RED,
+        (ZXColor::Purple, ZXBrightness::Bright) => ZX_BRIGHT_PURPLE,
+        (ZXColor::Green, ZXBrightness::Bright) => ZX_BRIGHT_GREEN,
+        (ZXColor::Cyan, ZXBrightness::Bright) => ZX_BRIGHT_CYAN,
+        (ZXColor::Yellow, ZXBrightness::Bright) => ZX_BRIGHT_YELLOW,
+        (ZXColor::White, ZXBrightness::Bright) => ZX_BRIGHT_WHITE,
+
+        // Normal Colors
+        (ZXColor::Blue, ZXBrightness::Normal) => ZX_NORMAL_BLUE,
+        (ZXColor::Red, ZXBrightness::Normal) => ZX_NORMAL_RED,
+        (ZXColor::Purple, ZXBrightness::Normal) => ZX_NORMAL_PURPLE,
+        (ZXColor::Green, ZXBrightness::Normal) => ZX_NORMAL_GREEN,
+        (ZXColor::Cyan, ZXBrightness::Normal) => ZX_NORMAL_CYAN,
+        (ZXColor::Yellow, ZXBrightness::Normal) => ZX_NORMAL_YELLOW,
+        (ZXColor::White, ZXBrightness::Normal) => ZX_NORMAL_WHITE,
     }
 }
 
