813496c5b46d58b0cfef7262170302054831ce2e
Juraj Michalek <juraj.michalek@gmail.com>
2023-12-18 17:04:53 +0100

    update to hal 0.14.0

--- m5stack-cores3/src/main.rs
+++ m5stack-cores3/src/main.rs
@@ -155,14 +155,15 @@ fn main() -> ! {
 
     let spi = Spi::new(
         peripherals.SPI3,
-        lcd_sclk,
-        lcd_mosi,
-        lcd_miso,
-        lcd_cs,
         60u32.MHz(),
         SpiMode::Mode0,
         &clocks,
-    )   .with_dma(dma_channel.configure(
+    ).with_pins(
+        Some(lcd_sclk),
+        Some(lcd_mosi),
+        Some(lcd_miso),
+        Some(lcd_cs),
+    ).with_dma(dma_channel.configure(
         false,
         &mut descriptors,
         &mut rx_descriptors,
