013cb112fc4a9935485d26fb9b49ad79170a9130
Juraj Michálek <juraj.michalek@espressif.com>
2024-01-31 09:12:39 +0100

    update esp32-c6 to embedded hal 1.0

--- esp32-c6/src/main.rs
+++ esp32-c6/src/main.rs
@@ -2,7 +2,7 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
-use spi_dma_displayinterface::spi_dma_displayinterface;
+use esp_display_interface_spi_dma::display_interface_spi_dma;
 use static_cell::make_static;
 
 use hal::{
@@ -21,12 +21,6 @@ use hal::{
     IO,
     systimer::SystemTimer,
     Uart
-};
-
-use spooky_embedded::{
-    embedded_display::{LCD_H_RES, LCD_V_RES},
-    // embedded_display::LCD_MEMORY_SIZE,
-    // controllers::{accel::AccelMovementController, composites::accel_composite::AccelCompositeController}
 };
 
 use esp_backtrace as _;
@@ -58,7 +52,7 @@ fn init_heap() {
 
 use embassy_time::{Duration, Ticker};
 
-use esp_bsp::lcd_gpios;
+use esp_bsp::{BoardType, lcd_gpios, DisplayConfig};
 
 #[main]
 async fn main(spawner: Spawner) -> ! {
@@ -72,8 +66,8 @@ async fn main(spawner: Spawner) -> ! {
     esp_println::logger::init_logger_from_env();
 
     info!("Starting up");
-    let embassy_timer = hal::timer::TimerGroup::new(peripherals.TIMG0, &clocks).timer0;
-    embassy::init(&clocks, embassy_timer);
+    let timer_group0 = hal::timer::TimerGroup::new(peripherals.TIMG0, &clocks);
+    embassy::init(&clocks, timer_group0);
 
     // ESP-NOW keyboard receiver
     let wifi_timer = SystemTimer::new(peripherals.SYSTIMER).alarm0;
@@ -143,10 +137,11 @@ async fn main(spawner: Spawner) -> ! {
         )
     );
 
-    let di = spi_dma_displayinterface::new_no_cs(2 * 256 * 192, spi, lcd_dc);
+    let di = display_interface_spi_dma::new_no_cs(2 * 256 * 192, spi, lcd_dc);
 
+    let display_config = DisplayConfig::for_board(BoardType::ESP32C6DevKitC1);
     let display = match mipidsi::Builder::ili9341_rgb565(di)
-        .with_display_size(LCD_H_RES, LCD_V_RES)
+        .with_display_size(display_config.h_res, display_config.v_res)
         .with_orientation(mipidsi::Orientation::Landscape(true))
         .with_color_order(mipidsi::ColorOrder::Rgb)
         .init(&mut delay, Some(lcd_reset))
